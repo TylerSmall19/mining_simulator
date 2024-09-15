7d4c0ddaa0206f85f8f647b00086f3f76832d1d2
Tyler Small <intrasolarmusic@gmail.com>
2024-09-15 23:48:14 +0900

    fix the domain var

--- client/src/game_logic/rendering/image_classes/APIImageSource.ts
+++ client/src/game_logic/rendering/image_classes/APIImageSource.ts
@@ -5,6 +5,6 @@ export class APIImageSource extends ImageSource {
    * image source will have the root API url prepended
    */
   constructor(imageSource: string, options?: ImageSourceOptions) {
-    super((process.env.REACT_APP_API_ROOT || '') + imageSource, options);
+    super((process.env.REACT_APP_API_ROOT_DOMAIN || '') + imageSource, options);
   }
 }
