6e6e99923d015c9abff590f50886aa633ff9c26f
Tyler Small <intrasolarmusic@gmail.com>
2024-09-15 23:48:00 +0900

    remove existing rendering utils in favor of new location on the model

--- client/src/game_logic/rendering/utils/renderingUtils.ts
+++ client/src/game_logic/rendering/utils/renderingUtils.ts
@@ -1,31 +1,3 @@
-import { lightBlue } from "@mui/material/colors";
-import { Actor, Color, Vector } from "excalibur";
-
-const moveActor = (actor: Actor, moveToPosition: Vector, actorSpeed: number) => {
-  const cursorActorName = 'moveCursor';
-  // clear movement actions
-  actor.actions.clearActions();
-  // clear all cursor actors
-  actor.scene?.actors.every((actor) => {
-    if(actor.name === cursorActorName)
-      actor.kill();
-    return true;
-  });
-  const moveCursor = new Actor({
-    color: Color.fromHex(lightBlue[50]),
-    height: 5,
-    width: 5,
-    x: moveToPosition.x,
-    y: moveToPosition.y,
-    name: cursorActorName
-  });
-  moveCursor.actions.blink(500, 500);
-  actor.scene?.add(moveCursor);
-  actor.actions.moveTo(moveToPosition, actorSpeed).toPromise().then(() => {
-    moveCursor.kill();
-  });
-}
-
-export const renderingUtils = {
-  moveActor
+export const renderUtils = {
+  
 }
