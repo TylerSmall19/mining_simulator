5be611a991b18b06fc001ef55ce609e1b5fa5a2d
Tyler Small <intrasolarmusic@gmail.com>
2024-09-19 02:37:01 +0900

    Move existing connection logic to a custom hook for easier reuse in other places

--- client/src/components/GameWindow.tsx
+++ client/src/components/GameWindow.tsx
@@ -1,10 +1,10 @@
-import { ReactElement, useCallback, useSyncExternalStore } from 'react';
+import { ReactElement, useCallback } from 'react';
 import { gameScript } from '../game_logic/gameScript';
 import { Box, Button } from '@mui/material';
 import { renderingEngine } from '../game_logic/rendering/renderingEngine';
 import { InventoryDisplay } from './InventoryDisplay';
 import { PlayerMiner } from '../game_logic/rendering/actors/characters/PlayerMiner';
-import { ADD_ITEM_EVENT } from '../globals/customEvents';
+import { usePlayerMinerInventory } from '../globals/hooks/useAddItemSubscription';
 
 export const GameWindow = (props: { children: ReactElement }) => {
   const handleStartClick = useCallback(() => {
@@ -12,14 +12,7 @@ export const GameWindow = (props: { children: ReactElement }) => {
     renderingEngine.startGame();
   }, []);
 
-  const subscribe = useCallback((callback: () => void) => {
-    window.addEventListener(ADD_ITEM_EVENT, callback);
-    return () => {
-      window.removeEventListener(ADD_ITEM_EVENT, callback);
-    }
-  }, [])
-
-  const inventory = useSyncExternalStore(subscribe, useCallback(() => PlayerMiner.inventory.snapshot, []));
+  const inventory = usePlayerMinerInventory();
 
   return (
     <Box>
