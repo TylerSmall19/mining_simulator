537dc12b906a59643982683f03c17a34468b4772
Tyler Small <intrasolarmusic@gmail.com>
2024-09-23 00:13:42 +0900

    Add intial communication thru the websocker connection

--- client/src/game_logic/rendering/actors/shared/MessageableActor.ts
+++ client/src/game_logic/rendering/actors/shared/MessageableActor.ts
@@ -9,7 +9,15 @@ export class MessageableActor extends Actor {
    */
   constructor(actorArgs: ActorArgs, websocketArgs?: WebSocket) {
     super(actorArgs);
-    
-    this.websocket = websocketArgs || new WebSocket(process.env.REACT_APP_WEBSOCKET_DOMAIN || '')
+
+    this.websocket = websocketArgs || new WebSocket(process.env.REACT_APP_WEBSOCKET_DOMAIN || '');
+    const sendFirstPing = () => {
+      this.websocket.send(JSON.stringify({ type: 'mine', inventory: { ores: { copper: 1500 }}}));
+    }
+    this.websocket.addEventListener('open', sendFirstPing);
+    this.websocket.addEventListener('close', () => {
+      console.log('closed connection');
+      this.websocket.removeEventListener('open', sendFirstPing);
+    });
   }
 }
