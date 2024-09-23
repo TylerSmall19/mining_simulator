f6be4904b94101e8cd69ccc00568e851cb20fb92
Tyler Small <intrasolarmusic@gmail.com>
2024-09-24 00:50:17 +0900

    Setup the playerChar repo

--- server/src/repositories/PlayerCharacterRepo.ts
+++ server/src/repositories/PlayerCharacterRepo.ts
@@ -1,7 +1,32 @@
+import { ObjectId } from "mongodb";
 import { BaseRepository } from "./BaseRepository";
 
+export type PlayerCharacter = {
+  characterID: string,
+  characterType: 'miner',
+  userID: string,
+  baseMiningAmount: number,
+  characterName: string
+}
+
+// DB ID: 66f18ba57587aa3fc91b7c15
 export class PlayerCharacterRepo extends BaseRepository {
-  constructor() {
-    super();
+  collectionName: string = 'playerCharacters';
+
+  async getCharacterByID(id: string): Promise<PlayerCharacter> {
+    if (!this.client)
+      throw new Error('No DB client found');
+
+    const myDB = this.client.db(this.dbName);
+    const gameSessionCollection = myDB.collection<PlayerCharacter>(this.collectionName);
+
+    const result = await gameSessionCollection.findOne({
+      _id: new ObjectId(id)
+    });
+
+    if (result)
+      return result;
+    else
+      throw new Error(`No character found with this ID: ${id}`);
   }
 }
