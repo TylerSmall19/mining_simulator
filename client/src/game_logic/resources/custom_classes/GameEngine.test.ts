73fac965651a973be4655e27ad11f72d948b59dc
Tyler Small <intrasolarmusic@gmail.com>
2024-09-30 18:15:08 +0900

    Add additional tests around setting and getting the active player

--- client/src/game_logic/resources/custom_classes/GameEngine.test.ts
+++ client/src/game_logic/resources/custom_classes/GameEngine.test.ts
@@ -1,3 +1,4 @@
+import { SET_ACTIVE_PLAYER_EVENT, SetActivePlayerEvent, SetActivePlayerShape } from "../../../globals/customEvents";
 import { PlayerDetails } from "../../../globals/types/ActivePlayerTypes";
 import { GameEngine } from "./GameEngine";
 
@@ -48,6 +49,23 @@ describe('GameEngine', () => {
     expect(engineToTest.activePlayer?._id).toEqual('test-foo');
   });
 
+  it.skip('sends a SetActivePlayerEvent upon storage', () => {
+    const expected = {
+      _id: 'test-foo',
+      playerName: 'lil bunny_foo-foo 4'
+    } as PlayerDetails;
+    expect.assertions(3);
+    const engineToTest = new GameEngine();
+    engineToTest.activePlayer = expected;
+    expect(engineToTest.activePlayer).toEqual(expected);
+    const callback = (e: any) => {
+      expect(e.detail?.activePlayer).toEqual(expected)
+    }
+    window.addEventListener(SET_ACTIVE_PLAYER_EVENT, callback);
+
+    window.removeEventListener(SET_ACTIVE_PLAYER_EVENT, callback);
+  });
+
   it('stores the object into localStorage with the activePlayerStorageKey', () => {
     jest.spyOn(Storage.prototype, 'setItem');
     const expected = {
@@ -57,7 +75,73 @@ describe('GameEngine', () => {
     expect(window.localStorage.setItem).not.toHaveBeenCalled();
     const engineToTest = new GameEngine({ activePlayer: expected });
     expect(window.localStorage.setItem).toHaveBeenCalledWith(engineToTest._activePlayerStorageKey, JSON.stringify(expected));
-    expect(engineToTest.activePlayer?._id).toEqual('test-foo');
+    expect(engineToTest.activePlayer).toEqual(expected);
+  });
+
+  describe('getActivePlayer', () => {
+    it('can fetch the player as an object from localStorage', () => {
+      const expected = {
+        _id: 'test-foo',
+        playerName: 'lil bunny_foo-foo'
+      } as PlayerDetails;
+      jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => JSON.stringify(expected));
+      const engineToTest = new GameEngine();
+      expect(engineToTest.activePlayer).toEqual(expected);
+      expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+    });
+
+    it('returns null but does not throw an exception when invalid JSON is passed to JSON.parse', () => {
+      jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => 'soooooo not a JSON object');
+      const engineToTest = new GameEngine();
+      expect(() => engineToTest.activePlayer).not.toThrow();
+      expect(engineToTest.activePlayer).toBeNull();
+    });
+
+    it('can fetch the player as an object when given to the constructor', () => {
+      const expected = {
+        _id: 'test-foo',
+        playerName: 'lil bunny_foo-foo'
+      } as PlayerDetails;
+      const engineToTest = new GameEngine({ activePlayer: expected });
+      expect(engineToTest.activePlayer).toEqual(expected);
+      expect(window.localStorage.getItem).toHaveBeenCalledTimes(1);
+    });
+
+    it('returns null when no object is present in localStorage', () => {
+      jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => null);
+      const engineToTest = new GameEngine();
+      expect(engineToTest.activePlayer).toBeNull();
+    });
+
+    it('returns null when given an undefined player', () => {
+      jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => null);
+      const engineToTest = new GameEngine({ activePlayer: undefined });
+      expect(engineToTest.activePlayer).toBeNull();
+    });
+
+    it('returns null when given a null player', () => {
+      jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => null);
+      // TS protects against this edge case, but the API response could be unexpected, so just in case
+      // @ts-ignore
+      const engineToTest = new GameEngine({ activePlayer: null });
+      expect(engineToTest.activePlayer).toBeNull();
+    });
+
+    it('returns the player pulled from localStorage even if an object is given in the constructor.', () => {
+      const expected = {
+        _id: 'test-foo',
+        playerName: 'lil bunny_foo-foo'
+      } as PlayerDetails;
+
+      const notExpected = {
+        _id: 'test-foo-FAKE',
+        playerName: 'lil bunny_foo-foo yoyo'
+      } as PlayerDetails;
+
+      jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => JSON.stringify(expected));
+      const engineToTest = new GameEngine({ activePlayer: notExpected });
+      expect(engineToTest.activePlayer).toEqual(expected);
+    });
   });
 
   // it('returns the object from memory when it cannot find it in localstorage', () => {
